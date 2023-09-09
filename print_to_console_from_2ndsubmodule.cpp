3c5ce7023408821490f857e4bd322ddaafe7706c
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2023-09-09 14:29:32 +0200

    :mag: added some test code

---
+++ print_to_console_from_2ndsubmodule.cpp
@@ -0,0 +1,7 @@
+#include <submodule/print_to_console_from_2ndsubmodule.hpp>
+
+#include <iostream>
+
+void a_2nd_submodule::print_to_console() {
+  std::cout << "a_2nd_submodule::print_to_console()" << std::endl;
+}
